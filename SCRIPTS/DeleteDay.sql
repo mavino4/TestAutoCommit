93682639640dae95965cfe72cd6ed72d2a909d6f
mavino4 <marco.antonio.vino@gmail.com>
2020-04-28 18:52:50 -0400

    fix gitignore

--- SCRIPTS/DeleteDay.sql
+++
@@ -1,2 +0,0 @@
-delete from daily_covid19_BO
-where daily_fecha = '2020-04-24'
--- SCRIPTS/ScriptAutoCommit.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-
-os.system("git add .")
-os.system('git commit -m "Autocommit {}"'.format(time.strftime("%Y%m%d %H%M")))
-os.system("git push -u origin master")
-
-
